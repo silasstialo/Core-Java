3bb77d0883cbcb5b24a1c0b7df4f5eb49c4ed2c6
silasstialo <silasstialo6@gmail.com>
2025-05-20 16:21:50 +0300

    added ColorChooser.java

---
+++ ColorChooser/README.md
@@ -0,0 +1,4 @@
+# Using a color chooser in Java
+A simple Java Swing application that demonstrates the use of JColorChooser within a dialog.
+A large-font button labeled “Select a Color” is placed at the bottom (BorderLayout.SOUTH). When clicked, it opens a color chooser dialog, and as the user picks a color, the background of the window dynamically updates in real-time.
+This example showcases interactive GUI event handling and color selection in Java Swing.
---
+++ ColorChooser/src/ColorChooser.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main extends JFrame {
+    public Main(){
+        setSize(1600, 900);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        Font largeFont = new Font("Arial", Font.PLAIN, 40);
+        setLayout(new BorderLayout());
+
+        //a button to invoke the color chooser
+        JButton colorChooserButton = new JButton("Select a Color");
+        colorChooserButton.setFont(largeFont);
+
+        colorChooserButton.addActionListener(_ -> {
+            //display a color chooser
+            //Color chosenColor = JColorChooser.showDialog(this, "Choose a color", Color.BLACK);
+            JColorChooser chooser = new JColorChooser();
+            JDialog dialog = JColorChooser.createDialog(
+                    this,
+                    "Background color",
+                    false,
+                    chooser,
+                    null,
+                    null
+            );
+            chooser.getSelectionModel().addChangeListener(_ -> {
+                getContentPane().setBackground(chooser.getColor());
+                System.out.println(chooser.getColor());
+            });
+            dialog.setVisible(true);
+        });
+
+        add(colorChooserButton, BorderLayout.SOUTH);
+
+    }
+
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new Main().setVisible(true));
+
+    }
+}
